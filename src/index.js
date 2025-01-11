4a50b43695778cc51c70b142a41d10faab790e47
Virender Parasariya <parasariya.1@iitj.ac.in>
2025-01-11 20:36:31 +0530

    Added utils for handling respsones

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
-connectDB()
+connectDB().then(
+    app.listen(process.env.PORT|| 8000,()=>{
+        console.log(`Server is running on port ${process.env.PORT}`)
+    })
+).catch((err) => {
+    console.error(`Error connecting to MongoDB: ${err.message}`);
+})
 
 dotenv.config(
     {
