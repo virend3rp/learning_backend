ab4e4eb269c192f19f48cba895b260b0a901da15
Virender Parasariya <parasariya.1@iitj.ac.in>
2025-01-14 01:31:06 +0530

    Get user channel profile

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -297,6 +297,57 @@ const updateUserCoverImage = asyncHandler(async(req, res) => {
         new ApiResponse(200, user, "Cover image updated successfully")
     )
 })
+
+const getUserChannelProfile=asyncHandler(async(req,res)=>{
+    const {username}=req.params
+
+    if(!username?.trim()){
+        throw new ApiError(400,"Username is required")
+    }
+    const channel=await User.aggregate([{
+        $match:{username:username?.toLowerCase()}
+    },{
+         $lookup:{
+            from:"subscriptions",
+            localField:"_id",
+            foreignField:"channel",
+            as:"subscribers",   
+         }
+    },{
+        $addFields:{
+            subscribersCount:{
+                $size:"$subscribers"
+            },
+            channelSubscribedTo:{
+                $size:"$subscribedTo"
+            },
+            isSubscrubed:{
+                $cond:{
+                    if:{$in:[req.user?._id,"$subscribers.subscriber"]},
+                    then:true,
+                    else:false
+                }
+            }   
+        }
+    },
+    {
+        $project :{
+            username:1,
+            avatar:1,
+            coverImage:1,
+            fullName:1,
+            subscribersCount:1,
+            channelSubscribedTo:1,
+            isSubscrubed:1,
+            email:1
+        }
+    }
+    ])
+    if (!channel?.length){
+        throw new ApiError(404,"Channel does not Exist");
+    }
+    return res.status(200).json(new ApiResponse(200,channel[0],"Channel Does not exists"));
+})
 export {
     registerUser,
     loginUser,
@@ -304,5 +355,8 @@ export {
     refreshAccessToken,
     changeCurrentPassword,
     getCurrentUser,
-    updateAccountDetails 
+    updateAccountDetails,
+    updateUserAvatar,
+    updateUserCoverImage
+    //... more routes for other functionalities 
 } 
